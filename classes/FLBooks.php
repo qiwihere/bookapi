655547e2106a47276370ef3a41cae05928a09fd7
Артем <qiwihere@gmail.com>
2019-01-30 17:52:56 +0300

    update

--- classes/FLBooks.php
+++ classes/FLBooks.php
@@ -11,10 +11,18 @@ class FLBooks
     static public function GetBookList($query)
     {
         $html = phpQuery::newDocument(file_get_contents('http://flibusta.is/booksearch?ask='.urlencode($query)));
-        $main_wrapper = phpQuery::newDocument($html->find('div#main-wrapper'));
-        $ul = $main_wrapper->find('ul');
-        echo('<pre>');
-        var_dump($ul);
-        echo('</pre>');
+        $main_wrapper = phpQuery::newDocument($html->find('ul'));
+
+        foreach($main_wrapper as $ul)
+        {
+            echo('<pre>');
+
+            $pqul = pq($ul);
+            var_dump($pqul->html());
+
+            echo('</pre>');
+        }
+
+
     }
 }
