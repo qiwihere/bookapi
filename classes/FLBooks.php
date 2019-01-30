9ecc1b69e884956b5a92ce00c93071e6fe2510d2
Артем <qiwihere@gmail.com>
2019-01-30 17:18:24 +0300

    update

---
+++ classes/FLBooks.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Артем
+ * Date: 30.01.2019
+ * Time: 16:51
+ */
+
+class FLBooks
+{
+    static public function GetBookList($query)
+    {
+        $html = file_get_html('http://flibusta.is/booksearch?ask='.urlencode($query));
+        print($html);
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?
 
-echo('hi');
+require_once('classes/FLBooks.php');
+require_once('lib/simple_html_dom.php');
+
+FLBooks::GetBookList('Девушка в поезде');
