9f181a48eb00c48362449e4357697f7710a283e8
Артем <qiwihere@gmail.com>
2019-01-30 16:08:51 +0300

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 import bookapi
 app = flask.Flask(__name__)
 
+
 @app.route('/search',methods=['GET'])
 def search():
     data = bookapi.get_book_list(flask.request.values.get('query'))
